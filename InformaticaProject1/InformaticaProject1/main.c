73789cc983ff5cd35e70f57c28194f211d6d86cb
GianlucaHeichTEsi <emistesi1998@hotmail.it>
2015-10-14 10:10:13 +0200

    Aggiunto progetto Xcode

---
+++ InformaticaProject1/InformaticaProject1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  InformaticaProject1
+//
+//  Created by Gianluca Heich Tesi on 14/10/15.
+//  Copyright © 2015 Gruppo Le Girelle. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
