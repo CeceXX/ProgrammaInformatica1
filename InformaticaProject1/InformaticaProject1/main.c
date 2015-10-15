7844b1937325cc573543fc5842a7ab77afad576b
CeceXX <hello@cesaredecal.com>
2015-10-15 06:37:51 +0200

    Aggiunta consegna in alto al codice

--- InformaticaProject1/InformaticaProject1/main.c
+++ InformaticaProject1/InformaticaProject1/main.c
@@ -1,15 +1,24 @@
-//
-//  main.c
-//  InformaticaProject1
-//
-//  Created by Gianluca Heich Tesi on 14/10/15.
-//  Copyright © 2015 Gruppo Le Girelle. All rights reserved.
-//
+/*
+ Gruppo 4: Belletta, De Cal, Tesi, Valeri
+ Viene richiesto un programma organizzato per funzioni che preveda un menu con la possibilità di scelta tra le seguenti operazioni:
+ 1. azzeramentodelvettore
+ 2. inserimentodituttiidatidatastiera
+ 3. inserimentodiunsolodatonellaprimaposizionecontentezero
+ 4. inserimento di tutti i dati in maniera casuale con numeri compresi tra duecento e
+ trecento
+ 5. lavisualizzazionedituttiidatipresentinelvettore
+ 6. l'inserimento dei numeri della serie geometrica
+ 7. la prodotto dei numeri di indice primo
+ 8. lasommadeirestidelledivisionepertredeinumeripari
+ 9. ricerca di un elemento nel vettore attraverso la ricerca sequenziale con sentinella
+ 10. ordinamento attraverso l'algoritmo del merge sort
+ L'array verrà dichiarato con una dimensione di 150 elementi, ma quando il programma parte, prima della visualizzazione del menu verrà richiesto di inserire il numero di elementi su cui operare
+
+*/
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+
     return 0;
 }
