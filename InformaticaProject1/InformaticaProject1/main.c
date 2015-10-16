f9cb79d3c4efa867e8522bbf19c48a741b416dd2
Cesare de Cal <hello@cesaredecal.com>
2015-10-16 14:09:58 +0200

    Aggiornamento

--- InformaticaProject1/InformaticaProject1/main.c
+++ InformaticaProject1/InformaticaProject1/main.c
@@ -1,6 +1,6 @@
 /*
  Gruppo 4: Belletta, De Cal, Tesi, Valeri
- Viene richiesto un programma organizzato per funzioni che preveda un menu con la possibilità di scelta tra le seguenti operazioni:
+ Viene richiesto un programma organizzato per funzioni che preveda un menu con la possibilita` di scelta tra le seguenti operazioni:
  1. azzeramento del vettore
  2. inserimento di tutti i dati da tastiera
  3. inserimento di un solo dato nella prima posizione contente zero
@@ -8,11 +8,11 @@
  trecento
  5. la visualizzazione di tutti i dati presenti nel vettore
  6. l'inserimento dei numeri della serie geometrica
- 7. la prodotto dei numeri di indice primo
+ 7. il prodotto dei numeri di indice primo
  8. la somma dei resti delle divisione per tre dei numeri pari
  9. ricerca di un elemento nel vettore attraverso la ricerca sequenziale con sentinella
  10. ordinamento attraverso l'algoritmo del merge sort
- L'array verrà dichiarato con una dimensione di 150 elementi, ma quando il programma parte, prima della visualizzazione del menu verrà richiesto di inserire il numero di elementi su cui operare
+ L'array verra` dichiarato con una dimensione di 150 elementi, ma quando il programma parte, prima della visualizzazione del menu verra` richiesto di inserire il numero di elementi su cui operare
  */
 
 #include <stdio.h>
@@ -50,12 +50,26 @@ void inserimentoSoloUnDatoSeZero(int array[], int lunghezza) {
     puts("Non ho trovato alcun elemento contenente 0.");
 }
 
+void prodottoElementiIndiceZero(int array[], int lunghezza) [
+	for (int i = 0; i < lunghezza; i++) {
+		//if (i % i == 0 && i != i) return false;
+
+	}
+}
+
 int main() {
     int myArray[150], maxNumero, inserimento;
     
     printf("Inserisci il numero massimo di numeri che vuoi che il vettore contenga: ");
-    scanf("%d", &maxNumero);
     
+    scanf("%d", &maxNumero);
+	
+	// controlla se il numero e' maggiore compreso di 1. Non esiste un'array con 0 elementi!
+	while (maxNumero < 1) {
+		printf("Riprova! Inserisci un numero maggiore di 0: ");
+		scanf("%d", &maxNumero);
+	}
+	
     while (1) {
         puts("--------------------------------------------------------------------------");
         puts("|  1  |  Azzeramento del vettore                                         |");
